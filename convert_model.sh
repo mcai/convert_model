27d628370aca3e4da1d1190733383690699b2212
Min Cai <min.cai.china@gmail.com>
2023-04-27 14:13:46 +0800

    Update convert_models.sh.

--- convert_model.sh
+++ convert_model.sh
@@ -1,10 +1,11 @@
 # Script to download a SD 1.5 model from Civitai and convert it to the format used by the diffuser
 # Example:
-# ./convert_model.sh realisticVisionV20_v20 29460
+# ./convert_model.sh models realisticVisionV20_v20 29460
 
 # Modify the following variables
-export model_name=$1 # Name of the model
-export civitai_model_id=$2 # ID of the model in Civitai
+export output_dir=$1 # Directory where the converted model will be saved
+export model_name=$2 # Name of the model
+export civitai_model_id=$3 # ID of the model in Civitai
 
 # Download model
 wget -c https://civitai.com/api/download/models/$civitai_model_id -O $model_name.safetensors
@@ -13,7 +14,5 @@ wget -c https://civitai.com/api/download/models/$civitai_model_id -O $model_name
 pip install -r requirements.txt
 
 # Convert model
-python convert_diffusers20_original_sd.py $model_name.safetensors models/$model_name --v1 --reference_model runwayml/stable-diffusion-v1-5
+python convert_diffusers20_original_sd.py $model_name.safetensors $output_dir/$model_name --v1 --reference_model runwayml/stable-diffusion-v1-5
 
-# Zip model
-zip -r models/$model_name.zip models/$model_name/
