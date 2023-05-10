2773e8d69853c9a74d14d2ccfc5b4ecbd64cc25b
MCAI <min.cai.china@gmail.com>
2023-05-10 13:24:24 +0800

    Update convert_model.sh

--- convert_model.sh
+++ convert_model.sh
@@ -4,15 +4,14 @@
 
 # Modify the following variables
 export output_dir=$1 # Directory where the converted model will be saved
-export model_name=$2 # Name of the model
-export civitai_model_id=$3 # ID of the model in Civitai
+export civitai_model_id=$2 # ID of the model in Civitai
 
 # Download model
-wget -c https://civitai.com/api/download/models/$civitai_model_id -O $model_name.safetensors
+wget -c https://civitai.com/api/download/models/$civitai_model_id -O model.safetensors
 
 # Install requirements
 pip install -r requirements.txt
 
 # Convert model
-python convert_diffusers20_original_sd.py $model_name.safetensors $output_dir --v1 --reference_model runwayml/stable-diffusion-v1-5
+python convert_diffusers20_original_sd.py model.safetensors $output_dir --v1 --reference_model runwayml/stable-diffusion-v1-5
 
